64a11a634d04b26fd8ce837f385bf8c45979cde4
Helen Gilissen <55249087+helengilissen@users.noreply.github.com>
2020-10-22 14:12:03 +0200

    added 8b-circles-everywhere

---
+++ js/sketch.js
@@ -0,0 +1,67 @@
+let x = 50;
+// let stap = 50;
+// let stop = 500;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(240);
+
+  while (x<windowHeight) {
+    fill (0,0,0,40)
+    ellipse (windowWidth /2, windowHeight /2 ,x,x);
+    x=x+40
+    }
+  x=50
+  }
+
+function draw() {
+}
+
+
+// hulpmiddellen
+// let x = 50;
+// let stap = 10;
+// let stop = 100;
+
+// function setup() {
+//   createCanvas(200, 200);
+//   background(240);
+// }
+
+// function draw() {
+//   while (x < stop) {
+//     line(x, 60, x, 80);
+//     x = x + stap;
+//   }
+//   x = 50;
+// }
+
+
+
+
+// ongeveer
+// let x = 50;
+// // let stap = 50;
+// // let stop = 500;
+
+// function setup() {
+//   createCanvas(500, 500);
+//   background(240);
+
+
+  
+
+  
+// }
+
+// function draw() {
+
+//   while (x<500) {
+  
+//     fill (200,200,200,50)
+//   ellipse (250, 250 ,x,x);
+//   x=x+10
+//   }
+
+// x=50
+// }
